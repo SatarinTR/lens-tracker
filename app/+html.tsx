7f939f614e76a8be96905b8beb7ef3416e8c0962
Gokalp Ozbek <gokalpozbek58@gmail.com>
2026-05-05 21:59:33 +0300

    arka plan tamamen siyah yapildi2

--- app/+html.tsx
+++ app/+html.tsx
@@ -7,12 +7,18 @@ export default function Root({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         
-        {/* İşte Apple'ın inadını kıran o sihirli kod */}
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        
+        {/* Arkada kalan o beyaz duvarı zorla siyah yapıyoruz */}
+        <style dangerouslySetInnerHTML={{ __html: `
+          html, body { 
+            background-color: #000000 !important; 
+          }
+        `}} />
         
         <ScrollViewStyleReset />
       </head>
-      <body>{children}</body>
+      <body style={{ backgroundColor: '#000000' }}>{children}</body>
     </html>
   );
 }
